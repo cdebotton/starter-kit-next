3e329803f810f28808173c19b34504cc7be9cc20
Christian de Botton <debotton@brooklynunited.com>
2016-11-08 13:08:08 -0500

    Add flow-typed

--- scripts/dev.server.js
+++ scripts/dev.server.js
@@ -1,13 +1,13 @@
 /* @flow */
 
-import Express from 'express';
+import express from 'express';
 import Debug from 'debug';
 import Webpack from 'webpack';
 import config from '../webpack.config.babel';
 
 import { DEV_PORT, DEBUG_TARGET } from '../src/config';
 
-const app = new Express();
+const app = express();
 const debug = new Debug(DEBUG_TARGET);
 const compiler = new Webpack(config);
 
@@ -22,7 +22,7 @@ app.use(require('webpack-hot-middleware')(compiler));
 
 app.listen(DEV_PORT, (err) => {
   if (err) {
-    debug(`🚫 ${err}`);
+    debug(`🚫 ${err.toString()}`);
   }
 
   debug(`✅ Development Server running on port ${DEV_PORT}`);
